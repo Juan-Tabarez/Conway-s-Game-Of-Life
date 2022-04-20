5b6eca7b2b7b2259dacfbad65e63ddf8a959c6ba
Juan-Tabarez <jtabarez6@gmail.com>
2022-04-20 20:31:27 -0300

    version1

---
+++ src/Library/Imprimir.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+
+namespace PII_Game_Of_Life
+{
+    public class Imprimir
+    {
+        public static void imprimirTablero(bool[,] b)
+        {
+            //bool[,] b =  Leer.leerArchivo(); //variable que representa el tablero
+            int width = b.GetLength(0); //variabe que representa el ancho del tablero
+            int height = b.GetLength(1); //variabe que representa altura del tablero
+
+            while (true)
+            {
+                Console.Clear();
+                StringBuilder s = new StringBuilder();
+                for (int y = 0; y<height; y++)
+                {
+                    for (int x = 0; x<width; x++)
+                    {
+                        if(b[x,y])
+                        {
+                            s.Append("|X|");
+                        }
+                        else
+                        {
+                            s.Append("___");
+                        }
+                    }
+                    s.Append("\n");
+                }
+                Console.WriteLine(s.ToString());
+                //=================================================
+                //Invocar método para calcular siguiente generación
+                //=================================================
+                b = Class1.procesarGeneracion(b);
+                Thread.Sleep(300);
+            }
+        }    
+    }
+}
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,8 +5,17 @@ namespace PII_Game_Of_Life
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            var hola = new bool[3,3] {
+                {true, false, false},
+                {false, true, true},
+                {true, true, false}
+            };
+
+            bool[,] tablero = Leer.leerArchivo(@"../../assets/board.txt");
+            
+            Imprimir.imprimirTablero(tablero);
+
         }
     }
 }
